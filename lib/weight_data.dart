e594de418cf995e992b20b67d3b57ad42740458f
ljm402968046 <402968046@qq.com>
2021-03-05 15:05:57 +0800

    no message

--- lib/weight_data.dart
+++ lib/weight_data.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:ljm_flutter/weights/MyScrollPhysics.dart';
 import 'package:ljm_flutter/weights/my_navigator2.dart';
 
 import 'page_data.dart';
@@ -12,7 +13,7 @@ class WeightData{
     PageData("Navigator2","页面内跳转",false,(context)=>MyNavigator2()),
     PageData("NestedScrollView","tabbar放和listview一起滚动",false,(context)=>MyNestedScrollView()),
     PageData("ScrollPhysics","ScrollPhysics并不是一个组件，它定义了可滚动组件的物理滚动特性。例如，当用户达到最大滚动范围时，是停止滚动，还是继续滚动。"
-        "\n滚动组件（CustomScrollView、ScrollView、GridView、ListView等）的physics参数表示此属性",false,(context)=>MyNestedScrollView())
+        "\n滚动组件（CustomScrollView、ScrollView、GridView、ListView等）的physics参数表示此属性",false,(context)=>MyScrollPhysics())
 
   ];
 
--- lib/weights/MyScrollPhysics.dart
+++ lib/weights/MyScrollPhysics.dart
@@ -1,8 +1,37 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class MyScrollPhysicsView extends StatelessWidget {
+class MyScrollPhysics extends StatelessWidget {
+  var _list = [
+    "AlwaysScrollableScrollPhysics"
+    ,"总是可以滑动"
+    ,"NeverScrollableScrollPhysics"
+    ,"禁止滚动"
+    ,"BouncingScrollPhysics"
+    ,"内容超过一屏 上拉有回弹效果"
+    ,"ClampingScrollPhysics"
+    ,"包裹内容 不会有回弹"
+    ,"FixedExtentScrollPhysics"
+    ,"滚动条直接落在某一项上，而不是任何位置，类似于老虎机，只能在确定的内容上停止，而不能停在2个内容的中间"
+    ,"PageScrollPhysics"
+    ,"用于PageView的滚动特性，停留在页面的边界"];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(),
+        title: Text("MyScrollPhysics"),
+      ),
+      body: ListView.builder(itemBuilder: (countext,index){
+        return
+          Container(
+            color: ThemeData.,
+            margin: EdgeInsets.all(10),
+            child: Card(child: ListTile(title: Text(_list[index])),),
+          );
+      },itemCount: _list.length,),
+    );
   }
 }
